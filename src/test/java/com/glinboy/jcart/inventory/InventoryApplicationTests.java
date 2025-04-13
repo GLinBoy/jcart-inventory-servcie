75b052da8bc6c1bc29f30166e8f18c8014b4a76f
GLinBoy <glinboy@live.com>
2025-04-13 17:42:52 +0000

    🕯 START: Initiated the project
    resolved #1

---
+++ src/test/java/com/glinboy/jcart/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glinboy.jcart.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
